19693f243e05842fe241b80a5542af28c8c6bd81
Marcos359 <80419062+Marcos359@users.noreply.github.com>
2023-11-25 20:59:30 +0100

    Update trainer.py

--- models/trainer.py
+++ models/trainer.py
@@ -44,12 +44,6 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
     tf.summary.scalar('train_loss', train_loss)
     copy_loss = tf.add_n(tf.get_collection('copy_loss'))
     tf.summary.scalar('copy_loss', copy_loss)  
-
-    ############### ERROR DE CONTROL ###############
-    #print("############### CONTROL de Pred")
-    #print(train_loss)
-    #print(pred)
-    #raise ValueError('Se ha llegado hasta el final!')
             
             
     # Learning rate settings
@@ -92,38 +86,25 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
         # This is where the training happens
         for i in range(epoch):
             start_time = time.time()
-            # x_batch son simplemente trozos de train de tamaño ["batch_size", 21, 228, 1]
-            # Además, el bucle se va a ejecutar len(inputs.get_data('train'))/batch_size, que para el caso estandar es 182-183 veces.
-            # n_his,hace referencia a la canrtidad de registros historicos que se van a utilizar para la predicción, por defecto se utilizan 13 de los posibles 21 registros posibles.
+            # x_batch are simply train chunks of size ["batch_size", 21, 228, 1].
+            # In addition, the loop is going to run len(inputs.get_data('train'))/batch_size, which for the standard case is 182-183 times.
+            # n_his, refers to the number of historical records to be used for the prediction, by default 13 of the possible 21 possible records are used.
             for j, x_batch in enumerate(
                     gen_batch(inputs.get_data('train'), batch_size, dynamic_batch=True, shuffle=True)):
-                #Descubrir que es merged y train_op.
                 # Train_op = Train optimizers
-                # merged = Más parámetros de configuración o seguimiento?
              
                 
-                # Entra datos para hacer predicciónes.
-                # Calcula el train_loss con las X (originalmente 8) filas que no utiliza.
-                # Como se mete el "train_op", aquí es donde se realiza la retropropagación, porque para eso necesitas un optimizador!!
-                # Recordatorio, de que el último de los x es utilizado como el y, es decir que si de [0:n_his + 1] van trece, 
-                    # se utilizarán 12 para realizar una predicción y esta se comparará al treceabo restante para calcular el loss.
+                # Enter data to make predictions.
+                # Calculates the train_loss with the X (originally 8) rows that it does not use.
+                # As the "train_op" is entered, this is where the backpropagation is done, because for that you need an optimizer!!!
+                # Reminder, that the last of the x's is used as the y, i.e. if from [0:n_his + 1] go thirteen, 
+                    # 12 will be used to make a prediction and this will be compared to the remaining thirteen to calculate the loss.
                 summary, w = sess.run([merged, train_op], feed_dict={x: x_batch[:, 0:n_his + 1, :, :], keep_prob: 1})
                 writer.add_summary(summary, i * epoch_step + j)
+
                 
                 
-                ############## CONTROL ###############
-                #print("############### CONTROL ###############")
-                #print(x_batch.shape)
-                #print(x_batch[0, 0:n_his + 1, 0, 0])
-                #print("############### CONTROL ###############")
-                #print(w)
-                #print(j)
-                #print(n_his + 1)
-                ############### ERROR DE CONTROL ###############
-                #raise ValueError('Se ha llegado hasta el final!')
-                
-                
-                # Cada 50 predicciones, se ve el progreso.
+                # Check every 50 iterations!
                 if j % 50 == 0: # if j % 50 == 0:
                     loss_value = \
                         sess.run([train_loss, copy_loss],
@@ -135,13 +116,7 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
             start_time = time.time()
             min_va_val, min_val = \
                 model_inference(sess, pred, inputs, batch_size, n_his, n_pred, step_idx, min_va_val, min_val)
-            
-            #print("############### CONTROL ###############")
-            #print([min_va_val, min_val])
-            #print("############### CONTROL ###############")
-            
-            # tmp_idx = n_pred - 1 
-            # De momento como no supera la validación minima de 40, no sale. 
+
             for ix in tmp_idx:
                 va, te = min_va_val[ix - 2:ix + 1], min_val[ix - 2:ix + 1]
                 print(f'Time Step {ix + 1}: '
@@ -150,14 +125,11 @@ def model_train(inputs, blocks, args, sum_path='./output/tensorboard'):
                       f'RMSE {va[2]:6.3f}, {te[2]:6.3f}.')
             print(f'Epoch {i:2d} Inference Time {time.time() - start_time:.3f}s')
             
-            ############### CONTROL ###############
-            # create a scalar summary
             summary = tf.Summary(value=[tf.Summary.Value(tag='Validation Loss', simple_value=min_va_val[2])])
 
             # add the summary to the writer
             writer.add_summary(summary, global_step=i)
-            ############### CONTROL ###############
-
+            
             if (i + 1) % args.save == 0:
                 model_save(sess, global_steps, 'STGCN')
         writer.flush()
